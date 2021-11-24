7c4b0a30e470cd695fdf615088db8758b3aed907
Govind Singh Rautela <94211239+grautela678@users.noreply.github.com>
2021-11-24 11:41:56 +0530

    Add files via upload

---
+++ 3_Implementation/inc/inc.h
@@ -0,0 +1,10 @@
+
+
+
+/************************************************
+ ** PROTOTYPES
+ ************************************************/
+char** createboard();
+void print(char** Board);
+int isdraw(char** Board);
+char winningmove(char** Board, int i, int j);
